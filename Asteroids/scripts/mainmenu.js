9f44f9a7b5527e2181b8a86f07a0839f23f55f92
Phil <phil@ifit.com>
2014-04-07 19:59:05 -0600

    Fixed where the AI screen would start no matter what screen you were in

--- Asteroids/scripts/mainmenu.js
+++ Asteroids/scripts/mainmenu.js
@@ -4,32 +4,52 @@ ASTEROIDS.screens['main-menu'] = (function() {
 	'use strict';
 	
 	function initialize() {
-		setTimeout(function(){ ASTEROIDS.game.showScreen('ai'); }, 10000);
+		var active = false;
+		setTimeout(function(){ 
+			if(!active){
+				ASTEROIDS.game.showScreen('ai');
+			}
+		}, 10000);
 		
 		// Setup each of menu events for the screens
 		document.getElementById('id-new-game').addEventListener(
 			'click',
-			function() { ASTEROIDS.game.showScreen('game-play'); },
+			function() { 
+				ASTEROIDS.game.showScreen('game-play');
+				active = true;
+			},
 			false);
 		
 		document.getElementById('id-settings').addEventListener(
 				'click',
-				function() {ASTEROIDS.game.showScreen('settings');},
+				function() {
+					ASTEROIDS.game.showScreen('settings');
+					active = true;
+				},
 				false);
 		
 		document.getElementById('id-high-scores').addEventListener(
 			'click',
-			function() { ASTEROIDS.game.showScreen('high-scores'); },
+			function() { 
+				ASTEROIDS.game.showScreen('high-scores');
+				active = true;
+			},
 			false);
 		
 		document.getElementById('id-help').addEventListener(
 			'click',
-			function() { ASTEROIDS.game.showScreen('help'); },
+			function() { 
+				ASTEROIDS.game.showScreen('help');
+				active = true;
+			},
 			false);
 		
 		document.getElementById('id-credits').addEventListener(
 			'click',
-			function() { ASTEROIDS.game.showScreen('credits'); },
+			function() { 
+				ASTEROIDS.game.showScreen('credits');
+				active = true;
+			},
 			false);
 	}
 	
