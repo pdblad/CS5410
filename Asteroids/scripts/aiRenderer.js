6e1ac629963b4454dd5809cd02f9beba871adc41
Phil <phil@ifit.com>
2014-04-07 19:42:40 -0600

    After the main menu screen is inactive for 10 seconds, it goes to our AI screen (right now the ship just sits in the middle, but everything seems to be working)

---
+++ Asteroids/scripts/aiRenderer.js
@@ -0,0 +1,403 @@
+/*jslint browser: true, white: true */
+/*global CanvasRenderingContext2D, ASTEROIDS */
+// ------------------------------------------------------------------
+//
+//
+// ------------------------------------------------------------------
+
+ASTEROIDS.aiGraphics = (function() {
+	'use strict';
+	
+	var canvas = document.getElementById('aiCanvas'),
+		context = canvas.getContext('2d');
+	ASTEROIDS.screenWidth = canvas.width = window.innerWidth;
+	ASTEROIDS.screenHeight = canvas.height = window.innerHeight;
+	
+	//
+	// Place a 'clear' function on the Canvas prototype, this makes it a part
+	// of the canvas, rather than making a function that calls and does it.
+	CanvasRenderingContext2D.prototype.clear = function() {
+		this.save();
+		this.setTransform(1, 0, 0, 1, 0, 0);
+		this.clearRect(0, 0, canvas.width, canvas.height);
+		this.restore();
+	};
+	
+	function clear() {
+		context.clear();
+	}
+	
+	function Texture(spec) {
+		var that = {};
+		
+		var resetCount = 0;
+
+		var wrap = function(){
+			var xMax = ASTEROIDS.screenWidth, yMax = ASTEROIDS.screenHeight;
+			// check x boundary
+			if (spec.center.x !== Math.abs(5000) && spec.center.y !== Math.abs(5000)){
+				if (spec.center.x > xMax) {
+					spec.center.x -= xMax;
+				} else if (spec.center.x < 0) {
+					spec.center.x += xMax;
+				}
+				// check y bounds
+				if (spec.center.y > yMax) {
+					spec.center.y -= yMax;
+				} else if (spec.center.y < 0) {
+					spec.center.y += yMax;
+				}
+			}
+		};
+		
+		that.getWidth = function(){
+			return spec.width;
+		};
+		
+		that.getHeight = function(){
+			return spec.height;
+		};
+		
+		that.getRadius = function(){
+			return spec.height/2;
+		};
+		
+		that.getX = function(){
+			return spec.center.x;
+		};
+		
+		that.getY = function(){
+			return spec.center.y;
+		};
+		
+		that.getPos = function(){
+			return spec.center;
+		};
+		
+		that.getRotation = function(){
+			return spec.rotation;
+		};
+		
+		that.getDx = function(){
+			return spec.dx;
+		};
+		
+		that.getDy = function(){
+			return spec.dy;
+		};
+		
+		that.getSize = function(){
+			return spec.size;
+		};
+		
+		that.getGunAngle = function(){
+			return{
+				x: Math.cos(spec.rotation),
+				y: Math.sin(spec.rotation)
+			};
+		};
+		
+		that.getGunPos = function(){
+			return {
+				x : spec.center.x+(spec.height/2)*Math.cos(spec.rotation), 
+				y : spec.center.y+(spec.height/2)*Math.sin(spec.rotation)
+			};
+		};
+		
+		that.getLeftThrusterPos = function(){
+			return {
+				x : spec.center.x+(spec.height/2)*-Math.cos(.5+spec.rotation), 
+				y : spec.center.y+(spec.height/2)*-Math.sin(.5+spec.rotation)
+			};
+		};
+		
+		that.getRightThrusterPos = function(){
+			return {
+				x : spec.center.x+(spec.height/2)*-Math.cos(-.5+spec.rotation), 
+				y : spec.center.y+(spec.height/2)*-Math.sin(-.5+spec.rotation)
+			};
+		};
+		
+		that.getThrusterStatus = function(){
+			return spec.fireThrusters;
+		};
+		
+		that.setThrusterStatus = function(status){
+			spec.fireThrusters = status;
+		};
+		
+		that.removeAsteroid = function(){
+			var oldX = that.getX(),
+				oldY = that.getY();
+			spec.center.x = 5000;
+			spec.center.y = 5000;
+			spec.rotation = 0;
+			spec.moveRate = 0;
+			spec.rotateRate = 0;
+			return {x: oldX, y: oldY};
+		};
+		
+		that.moveLeft = function(elapsedTime) {
+			spec.center.x -= spec.moveRate * (elapsedTime / 1000);
+		};
+		
+		that.moveRight = function(elapsedTime) {
+			spec.center.x += spec.moveRate * (elapsedTime / 1000);
+		};
+		
+		that.moveUp = function(elapsedTime) {
+			spec.center.y -= spec.moveRate * (elapsedTime / 1000);
+		};
+		
+		that.moveDown = function(elapsedTime) {
+			spec.center.y += spec.moveRate * (elapsedTime / 1000);
+			wrap();
+		};
+		
+		that.rotateRight = function(elapsedTime) {
+			spec.rotation += spec.rotateRate * (elapsedTime / 1000);
+			//console.log('Rotation: ' + spec.rotation);
+		};
+		
+		that.rotateLeft = function(elapsedTime) {
+			spec.rotation -= spec.rotateRate * (elapsedTime / 1000);
+			//console.log('Rotation: ' + spec.rotation);
+		};
+		
+		that.fireThrusters = function(elapsedTime){
+			spec.dx += Math.cos(spec.rotation) * 0.1;
+//			console.log("dx value: " + spec.dx);
+	        spec.dy += Math.sin(spec.rotation) * 0.1;
+//	        console.log("dy value: " + spec.dy);
+	        spec.fireThrusters = true;
+		};
+				
+		that.asteroidMovement = function(elapsedTime) {
+			if(spec.direction == 1){
+				that.rotateRight(elapsedTime);
+				that.moveRight(elapsedTime);
+				that.moveDown(elapsedTime);
+			}
+			else if(spec.direction == 2){
+				that.rotateRight(elapsedTime);
+				that.moveLeft(elapsedTime);
+				that.moveLeft(elapsedTime);
+				that.moveUp(elapsedTime);
+			}
+			else if(spec.direction == 3){
+				that.rotateLeft(elapsedTime);
+				that.moveRight(elapsedTime);
+				that.moveUp(elapsedTime);
+				that.moveUp(elapsedTime);
+				that.moveUp(elapsedTime);
+			}
+			else if(spec.direction == 4){
+				that.rotateLeft(elapsedTime);
+				that.moveLeft(elapsedTime);
+				that.moveLeft(elapsedTime);
+				that.moveDown(elapsedTime);
+			}
+			else{
+				that.rotateRight(elapsedTime);
+				that.moveRight(elapsedTime);
+				that.moveRight(elapsedTime);
+				that.moveUp(elapsedTime);
+			}
+
+			wrap();
+		};
+		
+		that.shipHit = function(){
+			spec.width = 0;
+			spec.height = 0;
+			spec.rotateRate = 0;
+			spec.dx = 0;
+			spec.dy = 0;
+			that.moveTo(5000, 5000);
+		};
+		
+		that.reset = function(elapsedTime){
+			spec.image = ASTEROIDS.images['images/USU-Logo.png'];
+			spec.center.x = ASTEROIDS.screenWidth/2;
+			spec.center.y = ASTEROIDS.screenHeight/2;
+			spec.width = 70;
+			spec.height = 70;
+			spec.rotation = -3.14;
+			spec.rotateRate = 3.14159;
+			spec.dx = 0;
+			spec.dy = 0;
+		};
+		
+		that.updatePos = function(elapsedTime){
+			spec.center.x += spec.dx;
+			spec.center.y += spec.dy;
+			//this is friction, uncomment if friction is wanted
+			//spec.dx *= .98;
+			//spec.dy *= .98;
+			wrap();
+		};
+		
+		that.updateEnemy = function(difficulty, elapsedTime){
+			//that.rotateRight(elapsedTime);
+			var randomnumber=Math.floor(Math.random()*4);
+			if(randomnumber === 0)
+				that.fireThrusters(elapsedTime);	
+			else if(randomnumber === 1){
+				for(var i = 0; i<10; i++)
+					that.rotateLeft(elapsedTime);
+			}
+			else if (randomnumber === 2){
+				for(var i = 0; i<10; i++)
+					that.rotateRight(elapsedTime);
+			}
+			else{}
+			that.updatePos(elapsedTime);
+		};
+		
+		that.moveTo = function(x, y) {
+			spec.center.x = x;
+			spec.center.y = y;
+		};
+		
+		that.draw = function() {
+			context.save();
+			
+			context.translate(spec.center.x, spec.center.y);
+			context.rotate(spec.rotation);
+			context.translate(-spec.center.x, -spec.center.y);
+			
+			context.drawImage(
+				spec.image, 
+				spec.center.x - spec.width/2, 
+				spec.center.y - spec.height/2,
+				spec.width, spec.height);
+			
+			context.restore();
+		};
+		
+		return that;
+	}
+	
+	//------------------------------------------------------------------
+	//
+	// Expose an ability to draw an image/texture on the canvas.
+	//
+	//------------------------------------------------------------------
+	function drawImage(spec) {
+		context.save();
+		
+		context.translate(spec.center.x, spec.center.y);
+		context.rotate(spec.rotation);
+		context.translate(-spec.center.x, -spec.center.y);
+		
+		context.drawImage(
+			spec.image, 
+			spec.center.x - spec.size/2, 
+			spec.center.y - spec.size/2,
+			spec.size, spec.size);
+		
+		context.restore();
+	}
+	
+    //code for rendering text
+    function Text(spec) {
+        var that = {};
+
+        that.getX = function(){
+        	return spec.pos.x;
+        };
+        
+        that.getScore = function(){
+        	return spec.text;
+        };
+        
+		that.updateScore20 = function(){
+			//Update score text by 20 for Large Asteroid
+			spec.text = spec.text + 20;
+		};
+		
+		that.updateScore50 = function(){
+			//Update score text by 50 for Medium Asteroid
+			spec.text = spec.text + 50;
+		};
+		
+		that.updateScore100 = function(){
+			//Update score text by 100 for Small Asteroid
+			spec.text = spec.text + 100;
+		};
+		
+		that.updateScore200 = function(){
+			//Update score text by 200 for Large Ship
+			spec.text = spec.text + 200;
+		};
+
+		that.updateScore1000 = function(){
+			//Update score text by 1000 for Small Ship
+			spec.text = spec.text + 1000;
+		};
+
+		that.nextLevel = function(){
+			spec.text = spec.text + 1;
+		};
+		
+		function measureTextHeight(spec) {
+            context.save();
+
+            context.font = spec.font;
+            context.fillStyle = spec.fill;
+            context.strokeStyle = spec.stroke;
+
+            var height = context.measureText('C').width;
+
+            context.restore();
+
+            return height;
+        }
+
+        function measureTextWidth(spec) {
+            context.save();
+
+            context.font = spec.font;
+            context.fillStyle = spec.fill;
+            context.strokeStyle = spec.stroke;
+
+            var width = context.measureText(spec.text).width;
+
+            context.restore();
+
+            return width;
+        }
+
+        that.drawText = function() {
+            context.save();
+
+            context.font = spec.font;
+            context.fillStyle = spec.fill;
+            context.strokeStyle = spec.stroke;
+            context.textBaseline = 'top';
+
+            context.translate(spec.pos.x + that.width / 2, spec.pos.y + that.height / 2);
+            context.rotate(spec.rotation);
+            context.translate(-(spec.pos.x + that.width / 2), -(spec.pos.y + that.height / 2));
+
+            context.fillText(spec.text, spec.pos.x, spec.pos.y);
+            context.strokeText(spec.text, spec.pos.x, spec.pos.y);
+
+            context.restore();
+        };
+
+        that.height = measureTextHeight(spec);
+        that.width = measureTextWidth(spec);
+        that.pos = spec.pos;
+
+        return that;
+    }
+
+
+	return {
+		clear : clear,
+		Texture : Texture,
+		Text : Text,
+		drawImage : drawImage
+	};
+}());
---
+++ Asteroids/scripts/artificialIntelligence.js
@@ -0,0 +1,562 @@
+/*jslint browser: true, white: true, plusplus: true */
+/*global ASTEROIDS, console, KeyEvent, requestAnimationFrame, performance */
+ASTEROIDS.screens['ai'] = (function() {
+	'use strict';
+	
+	var myKeyboard = ASTEROIDS.input.Keyboard(),
+		ship = null,
+		enemyShipEasy = null,
+		enemyShipHard = null,
+		leftThruster = null,
+		rightThruster = null,
+		shipExplosion1 = null,
+		shipExplosion2 = null,
+		shipExplosion3 = null,
+		asteroidExplosion = null,
+		hyperExplode = null,
+		missile = null,
+		asteroid = null,
+		asteroidsArray = [],
+		lifeArray = [],
+		count = 0,
+		pause = 0,
+		missileCount = 0,
+		numAsteroids = 0,
+		hyperCount = 0,
+		invincibleCount = 0,
+		newLife = 10000,
+		shootAudio = null,
+		ufoAudio = null,
+		thrustAudio = null,
+		explosionAudio = null,
+		shipExplodeAudio = null,
+		hyperAudio = null,
+		scoreText = null,
+		levelText = null,
+		levelNumText = null,
+		lives = null,
+		asteroidHit = false,
+		cancelNextRequest = false,
+		shipHit = false,
+		shipInvincible = false;
+	
+	var collisionDetected = function(object1, object2){
+		var object1Radius = object1.getRadius() - 15;	//Subtracted 15 to make up for ship not being exactly circular
+		var object2Radius = object2.getRadius();
+		var object1x = object1.getX();
+		var object1y = object1.getY();
+		var object2x = object2.getX();
+		var object2y = object2.getY();
+		
+		//Distances
+		var distanceX =  object1x - object2x;
+		var distanceY = object1y - object2y;
+		
+		var distance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+		
+		return (distance <= (object1Radius + object2Radius));
+	};
+	
+	var isSafe = function(x, y){
+		var safeRadius = 300;
+		var safeDistance = false;
+		for(var i = 0; i < asteroidsArray.length; i++){
+			var xDistance = x - asteroidsArray[i].getX();
+			var yDistance = y - asteroidsArray[i].getY();
+			safeDistance = (Math.sqrt((xDistance*xDistance) + (yDistance*yDistance)) > (safeRadius+asteroidsArray[i].getRadius()));
+		}
+		
+		return safeDistance;
+	};
+	
+	var hyperJump = function(){
+		if(ASTEROIDS.hyperReady){
+			hyperExplode.updatePos(ship.getX(), ship.getY());
+			hyperAudio.play();
+			for(var i = 0; i < 100; i++){
+				hyperExplode.create();
+			}
+			
+			var x = Random.nextRange(0, ASTEROIDS.screenWidth); 
+			var y = Random.nextRange(0, ASTEROIDS.screenHeight);
+	
+			if(isSafe(x, y)){
+				ship.moveTo(x, y);
+			}
+			else if(!isSafe(x, y)){
+				x = Random.nextRange(0, ASTEROIDS.screenWidth); 
+				y = Random.nextRange(0, ASTEROIDS.screenHeight);
+				hyperJump();
+			}
+			ASTEROIDS.hyperReady = false;
+			hyperCount = 0;
+		}
+	};
+	
+	var fireMissile = function(){
+		missile.updatePos({x: ship.getGunPos().x, y: ship.getGunPos().y}, 
+						//{x: ship.getGunAngle().x, y: ship.getGunAngle().y},
+						ship.getRotation()
+			);
+		missileCount++;
+		if(missileCount === 8){
+			shootAudio.play();
+			missile.create();
+			missileCount = 0;
+		}
+	};
+	
+	function initialize() {
+		console.log('game initializing...');
+		
+		numAsteroids = 3;
+		ASTEROIDS.hyperReady = true;
+
+		ship = ASTEROIDS.aiGraphics.Texture( {
+			image : ASTEROIDS.images['images/USU-Logo.png'],
+			center : { x : ASTEROIDS.screenWidth/2, y : ASTEROIDS.screenHeight/2 },
+			width : 70, height : 70,
+			rotation : -3.14,
+			moveRate : 200,			// pixels per second
+			rotateRate : 3.14159,	// Radians per second
+			dx : 0,
+			dy : 0,
+			fireThrusters : false
+		});
+		
+		enemyShipEasy = ASTEROIDS.aiGraphics.Texture({
+			image : ASTEROIDS.images['images/BYU-Logo.png'],
+			center : {x : Random.nextRange(50, ASTEROIDS.screenWidth), y : Random.nextRange(50, ASTEROIDS.screenHeight)},
+			width : 80, height : 80,
+			rotation : -3.14,
+			moveRate : 200,
+			rotateRate : 3.14159,
+			dx : 0,
+			dy : 0,
+			fireThrusters : false
+		});
+		
+		enemyShipHard = ASTEROIDS.aiGraphics.Texture({
+			image : ASTEROIDS.images['images/UofU-Logo.png'],
+			center : {x : Random.nextRange(50, ASTEROIDS.screenWidth), y : Random.nextRange(50, ASTEROIDS.screenHeight)},
+			width : 80, height : 80,
+			rotation : -3.14,
+			moveRate : 200,
+			rotateRate : 3.14159,
+			dx : 0,
+			dy : 0,
+			fireThrusters : false
+		});
+		
+		missile = gun({
+			image: ASTEROIDS.images['images/LaserBall.png'],
+			center: {x: ship.getGunPos().x, y: ship.getGunPos().y},
+			direction: {x: 0, y: 0},
+			speed: 350,
+			lifetime: 4,
+			}, ASTEROIDS.aiGraphics
+		);
+		
+
+		leftThruster = particleSystem({
+			image : ASTEROIDS.images['images/blueFire.png'],
+			center: {x: ship.getLeftThrusterPos().x, y: ship.getLeftThrusterPos().y},
+			size: {mean: 10, std: 4},
+			speed: {mean: 10, stdev: 2},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		rightThruster = particleSystem({
+			image : ASTEROIDS.images['images/blueFire.png'],
+			center : {x: ship.getRightThrusterPos().x, y: ship.getRightThrusterPos().y},
+			size: {mean: 10, std: 4},
+			speed : {mean: 10, stdev: 2},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		hyperExplode = particleSystem({
+			image : ASTEROIDS.images['images/star.png'],
+			center : {x: 0, y: 0},
+			size: {mean: 5, std: 4},
+			speed : {mean: 300, stdev: 10},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+
+		shipExplosion1 = particleSystem({
+			image : ASTEROIDS.images['images/fire.png'],
+			center : {x: 0, y: 0},
+			size: {mean: 30, std: 5},
+			speed : {mean: 300, stdev: 10},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		shipExplosion2 = particleSystem({
+			image : ASTEROIDS.images['images/smoke.png'],
+			center : {x: 0, y: 0},
+			size: {mean: 40, std: 5},
+			speed : {mean: 60, stdev: 40},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		shipExplosion3 = particleSystem({
+			image : ASTEROIDS.images['images/blueFire.png'],
+			center : {x: 0, y: 0},
+			size: {mean: 30, std: 5},
+			speed : {mean: 100, stdev: 10},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		asteroidExplosion = particleSystem({
+			image: ASTEROIDS.images['images/Asteroid2.png'],
+			center: {x: 0, y: 0},
+			size: {mean: 10, std: 5},
+			speed: {mean: 200, stdev: 10},
+			lifetime: {mean: 2, stdev: 1}
+			}, ASTEROIDS.aiGraphics
+		);
+		
+		shootAudio = audio({
+			sound: 'sounds/asteroids_shoot.wav',
+			duration: 0,
+			volume: .5
+		});
+		
+		ufoAudio = audio({
+			sound: 'sounds/asteroids_saucer.wav',
+			duration: 0,
+			volume: .5
+		});
+		
+		thrustAudio = audio({
+			sound: 'sounds/asteroids_thrust.wav',
+			duration: 0,
+			volume: .9
+		});
+		
+		explosionAudio = audio({
+			sound: 'sounds/depthCharge.wav',
+			duration: 0,
+			volume: .7
+		});
+		
+		shipExplodeAudio = audio({
+			sound: 'sounds/ship_explode.wav',
+			duration: 0,
+			volume: .7
+		});
+		
+		hyperAudio = audio({
+			sound: 'sounds/hyperJump.wav',
+			duration: 0,
+			volume: .5
+		});
+				
+		scoreText = ASTEROIDS.aiGraphics.Text({
+			text: 0,
+            font: '50px Arial, sans-serif',
+            fill: 'rgba(0, 0, 225, 0.5)',
+            stroke: 'rgba(255, 255, 255, 1)',
+            pos: {x: 20, y: 20},
+            rotation: 0
+		});
+		
+		levelText = ASTEROIDS.aiGraphics.Text({
+			text: "Level",
+            font: '50px Arial, sans-serif',
+            fill: 'rgba(0, 0, 225, 0.5)',
+            stroke: 'rgba(255, 255, 255, 1)',
+            pos: {x: 200, y: 20},
+            rotation: 0
+		});
+		
+		levelNumText = ASTEROIDS.aiGraphics.Text({
+			text: 1,
+            font: '50px Arial, sans-serif',
+            fill: 'rgba(0, 0, 225, 0.5)',
+            stroke: 'rgba(255, 255, 255, 1)',
+            pos: {x: 330, y: 20},
+            rotation: 0
+		});
+		
+		for(var i = 0; i < 3; i++){
+			lifeArray.push(
+				lives = ASTEROIDS.aiGraphics.Texture( {
+					image : ASTEROIDS.images['images/USU-Logo.png'],
+					center : { x : 450 + (i*75), y : 45 },
+					width : 50, height : 50
+				})
+			);
+		}
+		
+		for(var i = 0; i < numAsteroids; i++){
+			asteroidsArray.push(
+				asteroid = ASTEROIDS.aiGraphics.Texture( {
+					image : ASTEROIDS.images['images/Asteroid2.png'],
+					center : { x : Random.nextRange(50, ASTEROIDS.screenWidth), y : ASTEROIDS.screenHeight },
+					width : 125, height : 125,
+					rotation : 0,
+					moveRate : Math.abs(Random.nextGaussian(50, 10)),			// pixels per second
+					rotateRate : Random.nextRange(2, 6),	// Radians per second
+					size : 3,
+					direction : Random.nextRange(1,5)
+				})
+			);
+		}
+
+		//
+		// Create the keyboard input handler and register the keyboard commands
+		myKeyboard.registerCommand(KeyEvent.DOM_VK_ESCAPE, function() {
+			//
+			// Stop the game loop by canceling the request for the next animation frame
+			cancelNextRequest = true;
+			//
+			// Then, return to the main menu
+			ASTEROIDS.game.showScreen('main-menu');
+		});
+	}
+	
+	//This is the main update function where various frameworks can be updated
+	//
+	function gameUpdate(elapsedTime){
+		ASTEROIDS.aiGraphics.clear();
+
+		myKeyboard.update(elapsedTime);
+		
+		if(scoreText.getScore() >= newLife){
+			lifeArray.push(
+					lives = ASTEROIDS.aiGraphics.Texture( {
+						image : ASTEROIDS.images['images/USU-Logo.png'],
+						center : { x : 450 + (lifeArray.length*75), y : 45 },
+						width : 50, height : 50
+					})
+				);
+
+			newLife+=10000;
+		}
+		
+		//Start new level if all current asteroids (and UFO's) are destroyed
+		if(asteroidsArray.length == 0){
+			levelNumText.nextLevel();
+			numAsteroids++;
+			for(var i = 0; i < numAsteroids; i++){
+				asteroidsArray.push(
+					asteroid = ASTEROIDS.aiGraphics.Texture( {
+						image : ASTEROIDS.images['images/Asteroid2.png'],
+						center : { x : Random.nextRange(50, ASTEROIDS.screenWidth), y : ASTEROIDS.screenHeight },
+						width : 125, height : 125,
+						rotation : 0,
+						moveRate : Math.abs(Random.nextGaussian(50, 10)),			// pixels per second
+						rotateRate : Random.nextRange(2, 6),	// Radians per second
+						size : 3,
+						direction : Random.nextRange(1,5)
+					})
+				);
+			}
+		}
+		
+		for(var i = 0; i < asteroidsArray.length; i++){
+			asteroidsArray[i].asteroidMovement(elapsedTime);
+		}
+		
+		//asteroidHit === true then this code does stuff to make explosion there
+		asteroidHit = missile.findParticle(asteroidsArray);
+		if(asteroidHit.hit){
+			explosionAudio.play();
+			var x = asteroidHit.x, 
+				y = asteroidHit.y,
+				size = asteroidHit.size;
+			asteroidExplosion.updatePos(asteroidHit.x, asteroidHit.y);
+			for(var i = 0; i < 10; i++)
+				asteroidExplosion.create();
+			asteroidHit = false;
+			
+			//add new asteroids
+			//size 3 asteroids split into 3 smaller ones
+			if(size === 3){
+				scoreText.updateScore20();
+				for(i = 0; i < 3; i++){
+					asteroidsArray.push(ASTEROIDS.aiGraphics.Texture( {
+								image : ASTEROIDS.images['images/Asteroid2.png'],
+								center : { x : x, y : y },
+								width : 75, height : 75,
+								rotation : 0,
+								moveRate : Math.abs(Random.nextGaussian(50, 10)),			// pixels per second
+								rotateRate : Random.nextRange(2, 6),	// Radians per second
+								size : 2,
+								direction : Random.nextRange(1,5)
+							})
+						);
+				}
+			}
+			//size 2 asteroids split into 4 smaller ones
+			if(size === 2){
+				scoreText.updateScore50();
+				for(i = 0; i < 4; i++){
+					asteroidsArray.push(ASTEROIDS.aiGraphics.Texture( {
+								image : ASTEROIDS.images['images/Asteroid2.png'],
+								center : { x : x, y : y },
+								width : 30, height : 30,
+								rotation : 0,
+								moveRate : Math.abs(Random.nextGaussian(50, 10)),			// pixels per second
+								rotateRate : Random.nextRange(2, 6),	// Radians per second
+								size : 1,
+								direction : Random.nextRange(1,5)
+							})
+						);
+				}
+			}
+			
+			if(size === 1)
+				scoreText.updateScore100();
+		}
+		//end asteroid hit stuff
+		
+		//update the collisions between asteroid and ship
+		//we should definitely look at putting all of these for loops in functions...
+		if(!shipHit){
+			for(var i = 0; i < asteroidsArray.length; i++){
+				if(collisionDetected(ship, asteroidsArray[i])){
+					shipExplodeAudio.play();
+					lifeArray.pop();
+					if(lifeArray.length == 0){
+						//Game Over and Restart Game
+						ASTEROIDS.game.showScreen('credits');
+						cancelNextRequest = true;
+					}
+					
+					shipExplosion1.updatePos(ship.getX(), ship.getY());
+					shipExplosion2.updatePos(ship.getX(), ship.getY());
+					shipExplosion3.updatePos(ship.getX(), ship.getY());
+					for(var i = 0; i < 200; i++){
+						shipExplosion1.create();
+						if(i%2 === 0){
+							shipExplosion3.create();
+							shipExplosion2.create();
+						}
+						ship.shipHit();
+						shipHit = true;
+						shipInvincible = true;
+					}
+				}
+			}
+		}
+		else{
+			pause += elapsedTime;
+			invincibleCount += elapsedTime;
+			//disappear for 1.5 seconds
+			if(pause >= 1500 && shipInvincible){
+				ship.reset(elapsedTime);
+				shipInvincible = false;
+			}
+			if(invincibleCount >= 4000){
+				invincibleCount = pause = 0;
+				shipHit = false;
+			}
+		}
+		
+		//update all 3 ships
+		enemyShipEasy.updateEnemy('easy', elapsedTime);
+		enemyShipHard.updateEnemy('hard', elapsedTime);
+		ship.updatePos();
+
+		//only update thruster specs if the thruster button is hit
+		if(ship.getThrusterStatus()){
+			leftThruster.updatePos(ship.getLeftThrusterPos().x, ship.getLeftThrusterPos().y);
+			rightThruster.updatePos(ship.getRightThrusterPos().x, ship.getRightThrusterPos().y);
+			for(var i = 0; i < 10; i++){
+				leftThruster.create();
+				rightThruster.create();
+			}
+			thrustAudio.play();
+			ship.setThrusterStatus(false);
+		}
+		
+		//update particle systems
+		leftThruster.update(elapsedTime/1000);
+		rightThruster.update(elapsedTime/1000);
+		missile.update(elapsedTime/1000);
+		shipExplosion1.update(elapsedTime/1000);
+		shipExplosion2.update(elapsedTime/1000);
+		shipExplosion3.update(elapsedTime/1000);
+		asteroidExplosion.update(elapsedTime/1000);
+		hyperExplode.update(elapsedTime/1000);
+	}
+	
+	//This is the main render function where various frameworks are rendered
+	//
+	function gameRender(elapsedTime){
+		hyperCount += elapsedTime;
+		if (hyperCount > 1000){
+			ASTEROIDS.hyperReady = true;
+		}
+		ASTEROIDS.aiGraphics.clear();
+		
+		for(var i = 0; i < asteroidsArray.length; i++){
+			asteroidsArray[i].draw();
+		}
+				
+		leftThruster.render();
+		rightThruster.render();
+		
+		//draw all bullet particles
+		missile.render();
+		
+		//draw enemy ships
+		enemyShipEasy.draw();
+		enemyShipHard.draw();
+		
+		//draw ship explosion particles and asteroid explosion
+		shipExplosion1.render();
+		shipExplosion2.render();
+		shipExplosion3.render();
+		asteroidExplosion.render();
+		hyperExplode.render();
+		
+		//draw ship last to make exaust go behind it
+		ship.draw();
+		scoreText.drawText();
+		levelText.drawText();
+		levelNumText.drawText();
+		for(var i = 0; i < lifeArray.length; i++){
+			lifeArray[i].draw();
+		}
+	}
+	
+	//------------------------------------------------------------------
+	//
+	// This is the Game Loop function!
+	//
+	//------------------------------------------------------------------
+	function gameLoop(time) {
+		ASTEROIDS.elapsedTime = time - ASTEROIDS.lastTimeStamp;
+		ASTEROIDS.lastTimeStamp = time;
+		count++;
+
+		gameUpdate(ASTEROIDS.elapsedTime);
+		gameRender(ASTEROIDS.elapsedTime);		
+
+		if (!cancelNextRequest) {
+			requestAnimationFrame(gameLoop);
+		}
+	}
+	
+	function run() {
+		ASTEROIDS.lastTimeStamp = performance.now();
+		//
+		// Start the animation loop
+		
+		cancelNextRequest = false;
+		requestAnimationFrame(gameLoop);
+	}
+	
+	return {
+		initialize : initialize,
+		run : run
+	};
+}());
